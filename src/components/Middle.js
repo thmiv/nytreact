082efaf3bd79de2f2c7ff30b7ed77489ace2166e
thmiv <thmiv4@gmail.com>
2018-09-23 22:01:03 -0500

    file initialization

---
+++ src/components/Middle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Middle.css";
+
+const Middle = () => (
+    <div className="container">
+        <p>T E S T</p>
+    </div>
+);
+
+export default Middle;
